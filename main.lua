36c1731b9cfa96d70d9d6b81e58e1a1baddac3b7
i-e-b <ieb@me.com>
2016-07-23 09:22:34 +0100

    better smash

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local okToJump = false
 local acel = {x=0, y=0}
 local objects = {}
 local ball -- keep a reference for simple control
+local text = ""
 
 function love.load()
   world = love.physics.newWorld(0, 200, true)
@@ -14,22 +15,23 @@ function love.load()
   ball.b:setMass(20)
   ball.b:setFixedRotation( true ) -- makes friction work on simple circle (forces no rotation)
   ball.f:setRestitution(0.1)    -- make it less bouncy
-  ball.f:setFriction(0.4)
+  ball.f:setFriction(0.6)
 
   local water = NewSimpleThing(rect(1000,400), 0, 600, "static", {water=true})
   water.f:setSensor(true)
 
-  local floor1 = NewSimpleThing(rect(800, 10), 500, 400, "static", {floor=true})
+  local floor1 = NewSimpleThing(rect(400, 10), 200, 480, "static", {floor=true})
   floor1.b:setAngle(-0.4)
   floor1.f:setFriction(0.4)
   local floor2 = NewSimpleThing(rect(800, 10), -480, 400, "static", {floor=true})
   floor2.b:setAngle(0.4)
   floor2.f:setFriction(0.4)
+  local floor3 = NewSimpleThing(rect(800, 10), 700, 400, "static", {floor=true})
+  floor3.f:setFriction(0.8)
 
   local glass = NewSimpleThing(rect(1000, 2), 0, 400, "static", {floor=true, smash=300})
   glass.f:setFriction(0.01)
 
-  text = ""   -- we'll use this to put info text on the screen later
 end
 
 function circle(r) return love.physics.newCircleShape(r) end
@@ -138,13 +140,13 @@ end
 
 function beginContact(a, b, coll)
   x,y = coll:getNormal()
-  local ud_a = a:getUserData();
-  local ud_b = b:getUserData();
+  local ud_a = a:getUserData()
+  local ud_b = b:getUserData()
   local contactSpeed = getImpactSpeed(a:getBody(),b:getBody())
 
   if (ud_a.ball) or (ud_b.ball) then
     if (ud_a.floor) or (ud_b.floor) then
-      if (y < 0) then
+      if (y < 0) and (math.abs(x) < 0.4) then
         contactCount = contactCount + 1
         acel.x = x
         acel.y = y
@@ -153,14 +155,6 @@ function beginContact(a, b, coll)
     elseif (ud_a.water) or (ud_b.water) then
       inWater = inWater + 1
     end
-  end
-
-  -- TODO: this should be calculated from the postSolve callback,
-  -- so we use the force rather than the speed. http://www.iforce2d.net/b2dtut/sticky-projectiles
-  if (ud_a.smash) and (contactSpeed > ud_a.smash) then
-    a:getBody():destroy()
-  elseif (ud_b.smash) and (contactSpeed > ud_b.smash) then
-    b:getBody():destroy()
   end
 
   --text = text.."\n"..a:getUserData().." colliding with "..b:getUserData().." with a vector normal of: "..x..", "..y
@@ -177,32 +171,43 @@ function getImpactSpeed(a,b)
 end
 
 function endContact(a, b, coll)
-local ud_a = a:getUserData();
-local ud_b = b:getUserData();
+  local ud_a = a:getUserData()
+  local ud_b = b:getUserData()
 
-if (ud_a.ball) or (ud_b.ball) then
-  if (ud_a.floor) or (ud_b.floor) then
-    contactCount = contactCount - 1
-  elseif (ud_a.water) or (ud_b.water) then
-    inWater = inWater - 1
-  end
+  if (ud_a.ball) or (ud_b.ball) then
+    if (ud_a.floor) or (ud_b.floor) then
+      contactCount = contactCount - 1
+    elseif (ud_a.water) or (ud_b.water) then
+      inWater = inWater - 1
+    end
 
-  --text = text.."\nContacts: "..contactCount
-  if contactCount < 1 then
-    acel.x = 0
-    acel.y = -0.3 -- small amount of 'air' control
-    okToJump = false
+    --text = text.."\nContacts: "..contactCount
+    if contactCount < 1 then
+      acel.x = 0
+      acel.y = -0.3 -- small amount of 'air' control
+      okToJump = false
+    end
   end
-end
---text = text.."\n"..a:getUserData().." uncolliding with "..b:getUserData()
+  --text = text.."\n"..a:getUserData().." uncolliding with "..b:getUserData()
 end
 
 -- happens every timer tick for every touching contact
 -- except for sensors -- they only get begin/endContact
+-- here you can choose to 'cancel' the contact (for pass through)
 function preSolve(a, b, coll)
 end
 
--- this one too.
+-- happens every timer tick for every touching contact
+-- except for sensors -- they only get begin/endContact
+-- you can't cancel anymore, but you know the force of impact
 function postSolve(a, b, coll, normalimpulse, tangentimpulse)
-  if (math.abs(normalimpulse) > 100) then text = text.."\nimpact force: "..normalimpulse end
+  if (math.abs(normalimpulse) > 40) then text = text.."\nimpact force: "..normalimpulse end
+
+  local ud_a = a:getUserData();
+  local ud_b = b:getUserData();
+  if (ud_a.smash) and (normalimpulse > ud_a.smash) then
+    a:getBody():destroy()
+  elseif (ud_b.smash) and (normalimpulse > ud_b.smash) then
+    b:getBody():destroy()
+  end
 end
